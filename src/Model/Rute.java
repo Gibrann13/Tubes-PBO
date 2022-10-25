b758083ca286b877cd512ff5a4badfcb26d525ea
Gibran <>
2022-10-25 13:44:42 +0700

    COba Model

---
+++ src/Model/Rute.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+
+public class Rute implements Kota{
+    private int idRute;
+    private int tujuan;
+    private int keberangkatan;
+    private Kota kota;
+    //ini deklarasi juga kah?
+    
+}
