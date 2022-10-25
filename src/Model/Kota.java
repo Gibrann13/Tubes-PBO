8b5a7b3c0b7167f4b714517facc1a7b1dbac5ca1
Galang ikrar <galangikrrs@gmail.com>
2022-10-25 13:31:27 +0700

    COba Model

---
+++ src/Model/Kota.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+
+public interface Kota {
+    int BANDUNG = 1;
+    int JAKARTA = 2;
+    int DEPOK = 3;
+    int TANGGERANG = 4;
+    int BEKASI = 5;
+}
---
+++ src/Model/Tiket.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class Tiket {
+   private int idTiket;
+   private int idMobil;
+   private int idRute;
+   private Date date;
+   private String jam;
+   private double harga;
+   ArrayList<String> Rute;
+}
