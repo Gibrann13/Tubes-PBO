b2408b1e38df1409c599a8b4d4c45ba5bc18ceea
Arvin Situmorang <Arvin Situmorang@P1J92D9K-ARVIN-TUF-GAMING>
2022-11-25 10:27:36 +0700

    menu beli voucher (bagian submit)

--- src/Model/TransaksiVoucher.java
+++ src/Model/TransaksiVoucher.java
@@ -12,14 +12,19 @@ import java.util.Date;
  *
  * @author ASUS
  */
-public class TransaksiVoucher extends Transaksi{
+public class TransaksiVoucher extends Transaksi {
+
+    private int idTransaksi;
+    private int idVoucher;
+
     private ArrayList<Voucher> voucher = new ArrayList<>();
 
     public TransaksiVoucher() {
     }
 
-    public TransaksiVoucher(int idTransaksi, double totalPembayaran, Date date, String caraPembayaran, Member member) {
+    public TransaksiVoucher(int idTransaksi, double totalPembayaran, Date date, String caraPembayaran, Member member, int idVoucher) {
         super(idTransaksi, totalPembayaran, date, caraPembayaran, member);
+        this.idVoucher = idVoucher;
     }
 
     public ArrayList<Voucher> getVoucher() {
@@ -29,6 +34,21 @@ public class TransaksiVoucher extends Transaksi{
     public void setVoucher(ArrayList<Voucher> voucher) {
         this.voucher = voucher;
     }
-    
-    
+
+    public int getIdTransaksi() {
+        return idTransaksi;
+    }
+
+    public void setIdTransaksi(int idTransaksi) {
+        this.idTransaksi = idTransaksi;
+    }
+
+    public int getIdVoucher() {
+        return idVoucher;
+    }
+
+    public void setIdVoucher(int idVoucher) {
+        this.idVoucher = idVoucher;
+    }
+
 }
